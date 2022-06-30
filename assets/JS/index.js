e481222625693e36d73489d2dba86580afd04ad7
gavvahar <nihargavva@gmail.com>
2022-06-29 20:23:40 -0400

    corrected the path for the new folders

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -21,11 +21,11 @@ const inventory = newInventory();
 move(inventory).to(0, 0);
 
 // Create everything else
-move(newImage("assets/tree.png")).to(200, 450);
-move(newImage("assets/pillar.png")).to(350, 250);
-move(newImage("assets/pine-tree.png")).to(450, 350);
-move(newImage("assets/crate.png")).to(150, 350);
-move(newImage("assets/well.png")).to(500, 575);
-move(newItem("assets/sword.png")).to(500, 555);
-move(newItem("assets/shield.png")).to(165, 335);
-move(newItem("assets/staff.png")).to(600, 250);
+move(newImage("assets/images/PNG/tree.png")).to(200, 450);
+move(newImage("assets/images/PNG/pillar.png")).to(350, 250);
+move(newImage("assets/images/PNG/pine-tree.png")).to(450, 350);
+move(newImage("assets/images/PNG/crate.png")).to(150, 350);
+move(newImage("assets/images/PNG/well.png")).to(500, 575);
+move(newItem("assets/images/PNG/sword.png")).to(500, 555);
+move(newItem("assets/images/PNG/shield.png")).to(165, 335);
+move(newItem("assets/images/PNG/staff.png")).to(600, 250);
--- assets/JS/newPlayableCharacter.js
+++ assets/JS/newPlayableCharacter.js
@@ -1,28 +1,28 @@
 function newPlayableCharacter(x, y) {
-    const element = newImage('assets/green-character/static.gif')
-    element.style.zIndex = 1;
+  const element = newImage("assets/images/GIF/green-character/static.gif");
+  element.style.zIndex = 1;
 
-    function handleDirectionChange(direction) {
-        if (direction === null) {
-            element.src = `assets/green-character/static.gif`
-        }
-        if (direction === 'west') {
-            element.src = `assets/green-character/west.gif`
-        }
-        if (direction === 'north') {
-            element.src = `assets/green-character/north.gif`
-        }
-        if (direction === 'east') {
-            element.src = `assets/green-character/east.gif`
-        }
-        if (direction === 'south') {
-            element.src = `assets/green-character/south.gif`
-        }
+  function handleDirectionChange(direction) {
+    if (direction === null) {
+      element.src = `assets/images/GIF/green-character/static.gif`;
     }
-
-    move(element).withArrowKeys(x, y, handleDirectionChange)
-
-    return {
-        element: element
+    if (direction === "west") {
+      element.src = `assets/images/GIF/green-character/west.gif`;
     }
+    if (direction === "north") {
+      element.src = `assets/images/GIF/green-character/north.gif`;
+    }
+    if (direction === "east") {
+      element.src = `assets/images/GIF/green-character/east.gif`;
+    }
+    if (direction === "south") {
+      element.src = `assets/images/GIF/green-character/south.gif`;
+    }
+  }
+
+  move(element).withArrowKeys(x, y, handleDirectionChange);
+
+  return {
+    element: element,
+  };
 }
